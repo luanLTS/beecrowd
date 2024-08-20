235e8a9c4a9ec9ded200d4ccabfb9521aae444be
Luan Silva <luanlts@outlook.com>
2024-08-19 21:54:09 -0300

    feat(adhoc): 1379 problema com mediana e media [estrutura]

---
+++ adhoc/ex1379/Main.java
@@ -0,0 +1,19 @@
+package adhoc.ex1379;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        /**
+         * BEE 1379 - Problema com Mediana e Média
+         */
+
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+
+
+    }
+
+}
